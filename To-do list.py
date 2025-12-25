daf6830b5f6634f4026ac30f1bae7fb50928f339
Ankit Ghosh <ankit.ghosh1648@gmail.com>
2025-12-25 19:57:02 +0530

    Add files via upload

---
+++ To-do list.py
@@ -0,0 +1,47 @@
+def adding_tasks():
+    global a
+    a=[]
+    while True:
+        b=input("Enter the task you want to do: ")
+        if b == " ":
+            print("Invalid Input")
+            print("Enter A task")
+            break
+        a.append(b.upper())
+        c=input("Do you want to add more tasks(Yes/No): ")
+        if c.upper()=="NO":
+            print ("Your to-do list is")
+            print(a)
+            print ()
+            break
+        elif c.upper() not in ("YES","NO"):
+            print ("Invalid Input")
+            print ("Your to-do list is")
+            print (a)
+            print ()
+            break
+def completing_taks():
+    if a==[]:
+        print ("Your to-do list is empty")
+        return
+    while True:
+        d=input("Enter The Task you have completed from the To-do list: ")
+        if d.upper() in a:
+            a.remove(d.upper())
+            if a==[]:
+                print ("All tasks Completed")
+                break
+            else:
+                print("Taks left")
+                print(a)
+                e=input("Have you completed any more tasks(Yes/No): ")
+                if e.upper()=="NO":
+                    break
+                elif e.upper() not in ("YES","NO"):
+                    print ("Invalid Input")
+                    break
+        else:
+            print("Entered Task not in the to do list")
+adding_tasks()
+completing_taks()
+    
